efd13c74801ef21d72ff0768e00c364573d4f42a
YandereSpex <nanetoohakdog@gmail.com>
2025-08-27 23:40:16 +0800

    with git.js files

--- git.js
+++ git.js
@@ -1,1 +1,8 @@
-console.log('KJKJKJKJ');
+const clickbtn = document.querySelectorAll('.clickbtn');
+
+clickbtn.forEach((element) => {
+    element.addEventListener('click', () => {
+        console.log('Hello world');
+        console.log('Hello world 12345');
+    });
+});
---
+++ git2.js
@@ -0,0 +1,1 @@
+console.log("TULAD DATI HEHEHHEHE");
